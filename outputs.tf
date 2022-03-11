205f9579fdd0fbc41ad0f87a6d3f3465ce69816f
Manpreet Singh Nehra <manpreet@nehraconsultancy.eu>
2022-03-11 13:58:25 +0200

    add option to pass conditon

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,8 @@
 output "key" {
-  value = aws_kms_key.key
+  value       = aws_kms_key.key
   description = "Key id and arn map"
 }
 output "alias" {
-  value = aws_kms_alias.alias
+  value       = aws_kms_alias.alias
   description = "Alias id/arn and key id/arn map"
 }
--- variables.tf
+++ variables.tf
@@ -1,22 +1,23 @@
 variable "target_account_id" {
   description = "Account Id where the key would be used"
-  type = string
+  type        = string
 }
 variable "name" {
-  type = string
+  type        = string
   description = "Application/usage of the key"
 }
 variable "additional_permissions" {
   type = list(object(
-        {
-          identifiers = list(string)
-          actions     = list(string)
-          type        = string
-      }))
+    {
+      identifiers = list(string)
+      actions     = list(string)
+      type        = string
+      condition   = any
+  }))
   description = "Additional permissions, for cross account key access"
-  default = []
+  default     = []
 }
 variable "target_account_name" {
-  type = string
+  type        = string
   description = "Account name where the key would be used, it would added to all aliases"
 }
