f1eebf4451a392592018b930fdadbfce60d7356d
Manpreet Singh Nehra <manpreet@nehraconsultancy.eu>
2022-03-16 08:32:14 +0200

    Update outputs

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,14 @@
 output "key" {
-  value       = aws_kms_key.key
   description = "Key id and arn map"
+  value = {
+    id  = aws_kms_key.key.id
+    arn = aws_kms_key.key.arn
+  }
 }
 output "alias" {
-  value       = aws_kms_alias.alias
   description = "Alias id/arn and key id/arn map"
+  value = {
+    id  = aws_kms_alias.alias.id
+    arn = aws_kms_alias.alias.arn
+  }
 }
